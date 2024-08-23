9903ff79df1333c838c710bad52f63e791af0d91
lucasolopes <olopes.lucas567@gmail.com>
2024-08-22 21:21:25 -0300

    feat: Adicionando entrada e saida formatada de Card

---
+++ Shared/Responses/CardResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Responses
+{
+    public class CardResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int Position { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public DateTime? DateInit { get; set; }
+        public DateTime? DateCompleted { get; set; }
+        public string? Status { get; set; }
+        
+    }
+}
