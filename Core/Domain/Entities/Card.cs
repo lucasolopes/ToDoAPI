1a22b46966feb62a5225af5a39e3db66d7d95e72
lucasolopes <olopes.lucas567@gmail.com>
2024-08-22 21:22:05 -0300

    feat: adicionando conversao de card

--- Core/Domain/Entities/Card.cs
+++ Core/Domain/Entities/Card.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Shared.Requests;
+using Shared.Responses;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,12 +21,35 @@ namespace OnKanBan.Domain.Entities
         public DateTime LastUpdatedAt { get; set; } = DateTime.Now;
         public DateTime? DateInit { get; set; }
         public DateTime? DateCompleted { get; set; }
-        public StatusEnum? Status { get; set; }
+        public StatusEnum? Status { get; set; } = StatusEnum.ToDo;
 
         public string ListaId { get; set; }
         public Lista Lista { get; set; }
 
         public ICollection<Comments>? Comments { get; set; }
+
+        public Card() { }
+
+        public Card(CardRequest request) {
+            
+            this.Title = request.Title;
+            this.Description = request.Description;
+            this.Position = request.Position;
+            this.ListaId = request.ListaId;
+        }
+
+        public CardResponse Convert() => new CardResponse
+        {
+            Id = this.Id,
+            Title = this.Title,
+            Description = this.Description,
+            Position = this.Position,
+            CreatedAt = this.CreatedAt,
+            LastUpdatedAt = this.LastUpdatedAt,
+            DateInit = this.DateInit,
+            DateCompleted = this.DateCompleted,
+            Status = this.Status.ToString()
+        };
 
     }
 }
