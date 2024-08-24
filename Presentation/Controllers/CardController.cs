622e1876709f6bbbc4ef1f0b6d03f65ad69f5cf4
lucasolopes <olopes.lucas567@gmail.com>
2024-08-24 20:10:52 -0300

    feat: Criando Controllers Com CRUD

---
+++ Presentation/Controllers/CardController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/card")]
+    public class CardController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public CardController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _serviceManager.CardService().GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CardRequest cardRequest)
+        {
+            var result = await _serviceManager.CardService().CreateAsync(cardRequest);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, CardRequest cardRequest)
+        {
+            await _serviceManager.CardService().UpdateAsync(id, cardRequest);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _serviceManager.CardService().DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Presentation/Controllers/ListaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/lista")]
+    public class ListaController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public ListaController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _serviceManager.ListaService().GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ListaRequest listaRequest)
+        {
+            var result = await _serviceManager.ListaService().CreateAsync(listaRequest);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, ListaRequest listaRequest)
+        {
+            await _serviceManager.ListaService().UpdateAsync(id, listaRequest);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _serviceManager.ListaService().DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
